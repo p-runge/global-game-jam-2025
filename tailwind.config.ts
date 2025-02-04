e0b05c83c9574b6e8e51ed27384e55b33596f08a
Philipp Runge <p.runge@progani.com>
2025-02-04 23:53:27 +0100

    define color theme in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,25 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    screens: {},
+    colors: {
+      primary: {
+        darkest: "#011A7F",
+        darker: "#011A7F",
+        dark: "#0257E8",
+        DEFAULT: "#3198FA",
+        light: "#37CAFD",
+        lighter: "#57FAF9",
+        lightest: "#C3FCFB",
+      },
+      yellow: "#FDF36F",
+      orange: "#F86A46",
+      highlight: "#E354E1",
+      white: "#FDFCFA",
+      black: "#050737",
+      transparent: "transparent",
+    },
+
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
@@ -15,7 +34,6 @@ export default {
         card: "300px",
       },
     },
-    screens: {},
   },
   plugins: [],
 } satisfies Config;
